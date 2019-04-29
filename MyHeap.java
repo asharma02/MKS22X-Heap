5fc3026086dbda2b51e2d3004692c67226895eb6
Aryan Sharma <asharma00@stuy.edu>
2019-04-28 22:02:11 -0400

    wrote out first two functions, needed swap, wrote swap and push down

--- MyHeap.java
+++ MyHeap.java
@@ -1,13 +1,30 @@
 public class MyHeap {
 
+//swap needed
+private static void swap(int[] data, int first, int second) {
+  int og = data[first];
+	data[first] = data[second];
+  data[second] = og;
+}
 
 
 //- size  is the number of elements in the data array.
 //- push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided thatchild is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
 //- precondition: index is between 0 and size-1 inclusive
 //- precondition: size is between 0 and data.length-1 inclusive.
-private static void pushDown(int[]data,int size,int index){
-
+private static void pushDown(int[]data, int size, int index){ //RECURSIVE!!!
+    int c1 = 2*index + 1; //children
+    int c2 = 2*index + 2;
+  	if ((data[index] < data[c1] || data[index] < data[c2]) && !(c2 > size)) { //go until child 2 is past the size (at the bottom) and it is less than one of its cildren
+  		if (data[c1] > data[index]) { //if child1
+  		    swap(data, index, c1); //swap the numbers
+  		    pushDown(data, size, c1); //continue to push the numbers down
+  		}
+      else {//child 2
+  		    swap(data, index, c2);  //swap
+  		    pushDown(data, size, c2);//continue to push numbers down
+  		}
+  	}
 }
 
 
@@ -21,7 +38,7 @@ private static void pushUp(int[]data,int index) {
 
 
 //convert the array into a valid heap. [ should be O(n) ]
-public static void heapify(int[]){
+public static void heapify(int[] data){
 
 }
 
@@ -29,7 +46,7 @@ public static void heapify(int[]){
 // sort the array [ should be O(nlogn) ] :
 // converting it into a heap
 // removing the largest value n-1 times (remove places at end of the sub-array).
-public static void heapsort(int[]) {
+public static void heapsort(int[] data) {
 
 }
 
