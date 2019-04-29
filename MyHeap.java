afc0b88d8baa74da77a3bc32f9837cc312eacc05
Aryan Sharma <asharma00@stuy.edu>
2019-04-29 10:04:53 -0400

    fixed bug with push down

--- MyHeap.java
+++ MyHeap.java
@@ -15,8 +15,8 @@ private static void swap(int[] data, int first, int second) {
 private static void pushDown(int[]data, int size, int index){ //RECURSIVE!!!
     int c1 = 2 * index + 1; //children
     int c2 = 2 * index + 2;
-  	if ((data[index] < data[c1] || data[index] < data[c2]) && !(c2 > size)) { //go until child 2 is past the size (at the bottom) and it is less than one of its cildren
-  		if (data[c1] > data[index]) { //if child1
+  	if (!(c2 > size) && (data[index] < data[c1] || data[index] < data[c2])) { //go until child 2 is past the size (at the bottom) and it is less than one of its cildren
+  		if (data[c1] > data[c2]) { //if child1
   		    swap(data, index, c1); //swap the numbers
   		    pushDown(data, size, c1); //continue to push the numbers down
   		}
@@ -42,19 +42,29 @@ private static void pushUp(int[]data,int index) {//RECURSIVE!!!
 
 
 //convert the array into a valid heap. [ should be O(n) ]
-public static void heapify(int[] data){
+//public static void heapify(int[] data){
 
-}
+//}
 
 
 // sort the array [ should be O(nlogn) ] :
 // converting it into a heap
 // removing the largest value n-1 times (remove places at end of the sub-array).
-public static void heapsort(int[] data) {
+//public static void heapsort(int[] data) {
 
+//}
+
+public static void main(String[] args) {
+  int[] data = new int[] {71,90,40,60,50,51,80,20,30,91};
+  System.out.println(heapprinter.toString(data));
+  System.out.println();
+  System.out.println();
+  System.out.println();
+  System.out.println();
+  int size = data.length;
+  pushDown(data, size, 2);
+  System.out.println(heapprinter.toString(data));
 }
-
-
 
 
 
