79b63922891b42605f3a4e681d3a07bae1ffc557
Aryan Sharma <asharma00@stuy.edu>
2019-05-01 21:35:36 -0400

    fixed bug with pushdown, heapiy works

--- MyHeap.java
+++ MyHeap.java
@@ -15,23 +15,30 @@ private static void swap(int[] data, int first, int second) {
 private static void pushDown(int[]data, int size, int index){ //RECURSIVE!!!
     int c1 = 2 * index + 1; //children
     int c2 = 2 * index + 2;
-  	if ((c1 < size) && (data[index] < data[c1] || data[index] < data[c2])) { //go until child 2 is past the size (at the bottom) and it is less than one of its cildren
-  		if ((c2 >= size) || (data[c1] > data[c2])) { //if child1 //WHAT IF THERE IS NO C2
-  		    swap(data, index, c1); //swap the numbers
-  		    pushDown(data, size, c1); //continue to push the numbers down
-  		}
-      else {//child 2
-  		    swap(data, index, c2);  //swap
-  		    pushDown(data, size, c2);//continue to push numbers down
-  		}
-  	}
+    int larger;
+    if (c2 < size) { //if it has two children
+      if (data[c2] > data[c1]) { //if second one is larger
+        larger = c2; //set second child as larger
+      }
+      else {
+         larger = c1; //otherwise set first child
+      }
+    }
+    else {
+      larger = c1; //if only one child set that as larger
+    }
+
+    if(larger < size && data[larger] > data[index]) { //if it isnt the end and child is bigger
+      swap(data, index, larger); //swap
+      pushDown(data, size, larger); //recursive
+    }
 }
 
 
 
 //- push the element at index i up into the correct position. This will swap it with the parent node until the parent node is larger or the root is reached. [ should be O(logn) ]
 //- precondition: index is between 0 and data.length-1 inclusive.
-private static void pushUp(int[]data,int index) {//RECURSIVE!!!
+private static void pushUp(int[]data,int index) { //recursive
   int p = (index - 1) / 2;//parents
   	if (!(p < 0) && data[index] > data[p]) { //if number is bigger than parents and ur not at the top yet
   	    swap(data, index, p); //swap numbers
@@ -57,7 +64,7 @@ public static void heapsort(int[] data) {
 }
 
 public static void main(String[] args) {
-  int[] data = new int[] {1,90,40,50,60,51,80,20,30,91};
+  int[] data = new int[] {1,90,40,30,60,51,80,20,50,91};
   System.out.println(heapprinter.toString(data));
   System.out.println();
   System.out.println();
